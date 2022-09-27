ba533d02d47b42bb9d47cded96fed0f0e06bf4ce
Sri Reeha Nidamarty <reehanidamarty@Sris-MBP.lan>
2022-09-26 21:35:12 -0400

    Making change to header

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,12 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Fib Calculator
           </a>
           <Link to="/">Home</Link>
